922ec777a93e86041b348e2d2949193eace2fa46
aluno <aluno@ifpe>
2023-03-23 15:57:30 -0300

    enviando os arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiGuilhermeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiGuilhermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiGuilhermeApplication.class, args);
+	}
+
+}
