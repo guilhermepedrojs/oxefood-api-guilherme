d750c4c4f5848379ca67945135388e5808d5d3d0
aluno <aluno@ifpe>
2023-04-03 15:04:57 -0300

    Implementando a Inclusão (parte1)

---
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
@@ -0,0 +1,36 @@
+package br.com.ifpe.oxefood.api.cliente;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteRequest {
+
+   private String nome;
+
+   private LocalDate dataNascimento;
+
+   private String cpf;
+
+   private String foneCelular;
+
+   private String foneFixo;
+
+   public Cliente build() {
+
+       return Cliente.builder()
+               .nome(nome)
+               .cpf(cpf)
+               .foneCelular(foneCelular)
+               .foneFixo(foneFixo)
+               .build();
+   }
+}
